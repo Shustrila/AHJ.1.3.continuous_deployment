4c102eff32436549b3f4f2b6a1aa1bb6c0272629
Александр Махалов <aleksandrmahalov@MacBook-Aleksandr.local>
2019-03-30 21:48:47 +0300

    add

--- src/js/__tests__/module.test.js
+++ src/js/__tests__/module.test.js
@@ -3,6 +3,6 @@ const Module = require('../module');
 describe('TEST: module', () => {
     test('all good', () => {
         const module = new Module();
-        expect(module.start()).toBe('name:default, type:defaul');
+        expect(module.start()).toBe('name:default, type:default');
     });
 });
